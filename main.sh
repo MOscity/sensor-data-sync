5f711f8f1d419878ef2c09f34eb6880301df321d
Matthias Oscity <matthias.oscity@me.com>
2022-11-12 21:03:04 +0100

    Added additional options to shell script
    kwargs, interval, units and export_dir.
    Renamed intervals to csv_intervals.

--- main.sh
+++ main.sh
@@ -5,55 +5,100 @@
 #  Adjust these parameters before you run the script
 #
 ##########################################################
-CONFIG_FILE=configs/config_10_Local_Testing.ini
+CONFIG_FILE=configs/config_template_local_testing.ini
 INTERVALS_FILE=configs/intervals.csv
+EXPORT_DIR="/mnt/d/FHNW/2021_ComPAS/Path_With_No_Spaces/"
 
 ##########################################################
-#
+
 #   End of inputs
 #
 ##########################################################
 
-# Read Configs.ini
-source <(grep = ${CONFIG_FILE} | sed 's/ *= */=/g')
-echo 'Ignore above warnings :)'
-echo '--------------------'
-echo 'Testing if link to config works (should not be empty):'
-echo $exportDirectoryPathString
-echo 'If the line above is empty, you have errors and not warnings :D'
-echo '--------------------'
+##########################################################
+#
+#   # Examples:
+#
+#   # Run the script with the config file as defined in __main__.py:
+#   ./main.sh
+#
+#   # Run the script with the config file as here defined above:
+#   ./main.sh ini
+#
+#   # Run the script with the config file and intervals file as defined here above:
+#   ./main.sh ini csv_intervals
+#
+#   # Run the script using the config file but other time intervals.
+#   # Use with --inifile, (--csv_intervals) or (--interval and --units), --export_dir
+#   # If no options specified, it executes the same code as ./main.shs
+#   ./main.sh kwargs [options]
+#
+#   # Examples:
+#   ./main.sh kwargs --interval=3
+#   ./main.sh kwargs --interval 2 --units 'min'
+#   ./main.sh kwargs --interval 1 --units 'min' --export_dir='C:/mypath'
+#   ./main.sh kwargs --units 'hours' --export_dir 'C:/mypath'
+#   ./main.sh kwargs --interval 3 --export_dir 'C:/mypath'
+#   ./main.sh kwargs --export_dir='C:/mypath'
+#   ./main.sh kwargs --csv_intervals='configs/intervals.csv' --export_dir='C:/mypath'
+#
+##########################################################
+
+echo '##########+---------------+##########'
 # works
 # Wrapper function to print the command being run
 function run {
     # shellcheck disable=SC2145
-    echo "$ $@"
+    echo "$ $@"s
     "$@"
-    echo '####################'
+    echo '##########+---------------+##########'
 }
 
 # First the ones with 2 modes (second is)
-if [[ $1 == 'ini' && $2 == 'intervals' ]]; then
+if [[ $1 == 'ini' && $2 == 'csv_intervals' ]]; then
     echo "Running Sensor-Data-Sync with other config file and other intervals."
-    echo 'Config File:' $CONFIG_FILE
-    echo 'Intervals File:' $INTERVALS_FILE
-    echo '--------------------'
+    # Read Configs.ini
+    source <(grep = ${CONFIG_FILE} | sed 's/ *= */=/g')
+    echo 'Ignore above warnings :)'
+    echo 'Test if link to config file works (this should not be empty):'
+    echo '+++++++++++'
+    echo 'Export File Name is:' $exportFileNameString
+    echo '+++++++++++'
+    echo 'If export file name is empty, you will have errors. Maybe check your path.'
+    echo '>>'
+    echo '  Selected Config File:' $CONFIG_FILE
+    echo '  Selected Intervals CSV File:' $INTERVALS_FILE
+    echo '--'
     run python __main__.py --inifile=${CONFIG_FILE} \
-        --intervals=${INTERVALS_FILE}
+        --csv_intervals=${INTERVALS_FILE}
 
 elif [[ $1 == 'ini' && $2 == 'special' ]]; then
-    echo "Not supported yet."
-    echo '--------------------'
+    echo "Not supported yet. Create your own. :)"
+    echo '--'
 
 # Then the ones with only 1 mode (no second mode given)
 elif [[ $1 == 'ini' ]]; then
-    echo "No 2nd argument passed. Only '$1':"
     echo "Running Sensor-Data-Sync with other config file."
-    echo 'Config File:' $CONFIG_FILE
-    echo '--------------------'
+    # Read Configs.ini
+    source <(grep = ${CONFIG_FILE} | sed 's/ *= */=/g')
+    echo 'Ignore above warnings :)'
+    echo 'Test if link to config file works (this should not be empty):'
+    echo '+++++++++++'
+    echo 'Export File Name is:' $exportFileNameString
+    echo '+++++++++++'
+    echo 'If export file name is empty, you will have errors. Maybe check your path.'
+    echo '>>'
+    echo '  Selected Config File:' $CONFIG_FILE
+    echo '--'
     run python __main__.py --inifile=${CONFIG_FILE}
+
+elif [[ $1 == 'kwargs' ]]; then
+    echo "Running Sensor-Data-Sync, passing custom arguments."
+    echo '--'
+    run python __main__.py --custom_args ${@:2}
 
 else
     echo "No arguments passed."
-    echo '--------------------'
+    echo '--'
     run python __main__.py
 fi
